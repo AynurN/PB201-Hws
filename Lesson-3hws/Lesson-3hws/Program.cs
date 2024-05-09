86eb450ee23d1a3136fdfd2bef3c66a7f2f29b5f
Aynur Nazarli <aynurnazarli2005@gmail.com>
2024-05-09 20:27:45 +0400

    first commit

---
+++ Lesson-3hws/Lesson-3hws/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Lesson_3hws
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task1
+            for (int i = 1; i <= 100; i++)
+                if (i % 3 == 0 && i % 5 == 0) Console.WriteLine(i);
+
+            //Task2
+            int x;
+            x = 80;
+            if (x < 65)
+                Console.WriteLine("Kesildin");
+            else if (x >= 65 && x < 85)
+                Console.WriteLine("Adi Diplom");
+            else if (x >= 85 && x < 95)
+                Console.WriteLine("Sheref Diplomu");
+            else
+                Console.WriteLine("Yuksek sheref Diplomu");
+        }
+    }
+}
+
+    
+
+
