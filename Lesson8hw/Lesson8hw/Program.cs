a49dabc25ad97760ba36272f2e4e8adea73fd7c7
Aynur Nazarli <aynurnazarli2005@gmail.com>
2024-05-19 18:32:20 +0400

    lesson 8 added

---
+++ Lesson8hw/Lesson8hw/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Lesson8hw
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SumOfEven(10));
+            PrintSumOfEven(10);
+
+            Console.WriteLine(EraseTheSpaces("CodeAcademy"));
+            PrintTheFirstLetters("Code Academy");
+
+
+
+        }
+        //Task1
+        static int SumOfEven(int n)
+        { int sum = 0;
+            for (int i = 0; i < n; i++)
+                if (i % 2 == 0) sum += i;
+            return sum;
+        }
+        static void PrintSumOfEven(int n)
+        {
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+                if (i % 2 == 0) sum += i;
+            Console.WriteLine(sum);
+
+        }
+        //Task2
+        static string EraseTheSpaces(string s)
+        {
+            string snew = "";
+            for (int i = 0; i < s.Length; i++)
+                if (s[i] != ' ') snew += s[i];
+            return snew;
+        }
+        //Task3
+        static void PrintTheFirstLetters(string s)
+        {
+            for(int i=0; i<s.Length; i++)
+            {
+                if (i==0 ||s[i - 1] == ' ') Console.WriteLine(s[i]);
+
+            }
+        }
+
+    }
+}
