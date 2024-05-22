7bc448550a441d191d2abff5ce71ecdbf9789751
Aynur Nazarli <aynurnazarli2005@gmail.com>
2024-05-22 08:14:55 +0400

    Added lesson10

---
+++ Lesson10hw(Class, Inheritance, Constructor)/Lesson10hw(Class, Inheritance, Constructor)/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson10hw_Class__Inheritance__Constructor_
+{//Task2
+    internal class Car
+    {
+        public string Brand;
+        public string Model;
+        public int CurrentFuel;
+        public int FuelFor1Km;
+        public int Millage;
+        
+        public void Drive(int km)
+        {
+            if (CurrentFuel - FuelFor1Km * km > 0)
+            {
+                CurrentFuel-= FuelFor1Km * km;
+                Millage += km;
+            }
+            else
+            {
+                Console.WriteLine("There is not enough fuel");
+            }
+        }
+        public void ShowAllProperities()
+        {
+            Console.WriteLine($"Brand:{Brand}, Model:{Model}, CurrentFuel:{CurrentFuel}, FuleFor1Km:{FuelFor1Km}, Millage:{Millage}");
+        }
+    }
+}
---
+++ Lesson10hw(Class, Inheritance, Constructor)/Lesson10hw(Class, Inheritance, Constructor)/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Lesson10hw_Class__Inheritance__Constructor_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
+            //int[] arr2 = Resize(arr, 10);
+            //foreach (int i in arr2)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            Car car=new Car();
+            car.Brand = "BMW";
+            car.Model = "M5";
+            car.CurrentFuel = 350;
+            car.FuelFor1Km = 3;
+            car.Millage = 2000;
+            car.Drive(50);
+            car.ShowAllProperities();
+
+        }
+        //Task1
+        static int[] Resize(int[] oldarr, int size)
+        {
+            int[] newarr = new int[size];
+            if (size <= oldarr.Length)
+            {
+                for (int i = 0; i < size; i++)
+                    newarr[i] = oldarr[i];
+            }
+            else
+            {
+                for (int i = 0; i < oldarr.Length; i++)
+                    newarr[i] = oldarr[i];
+
+            }
+            return newarr;
+
+        }
+    }
+}
